de044cf38ed27567a46a693985ea7a94cb184e22
sergeyvorobey <phanta2408@yandex.ru>
2019-12-26 19:29:14 +0300

    Initial Commit

---
+++ TableViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewController
+//
+//  Created by Sergey Vorobey on 26/12/2019.
+//  Copyright © 2019 Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
